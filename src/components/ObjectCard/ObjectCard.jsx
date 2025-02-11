26791ff276e916a354d5487cc5b18b8421b37730
jerhum <jerhum@msn.com>
2025-02-11 12:55:15 +0100

    modif page ObjectsList

--- src/components/ObjectCard/ObjectCard.jsx
+++ src/components/ObjectCard/ObjectCard.jsx
@@ -1,3 +1,4 @@
+import { FaSquareArrowUpRight } from 'react-icons/fa6';
 import './ObjectCard.scss'
 
 import React from 'react';
@@ -5,9 +6,11 @@ import React from 'react';
 function ObjectCard({nom, adresse, numeroObjet}) {
     return (
         <div className='ObjectCardContainer'>
-            <p className="nom">{nom}</p>
-            <p className="adresse">{adresse}</p>
-            <p className="numeroObjet">{numeroObjet}</p>
+            <p className="adresse">{adresse}<FaSquareArrowUpRight /></p>
+            <div>
+                <p className="nom">{nom}</p>
+                <p className="numeroObjet">{numeroObjet}</p>
+            </div>
         </div>
     );
 }
--- src/pages/ObjectsList/ObjectsList.jsx
+++ src/pages/ObjectsList/ObjectsList.jsx
@@ -1,17 +1,38 @@
-import { FaArrowAltCircleLeft } from 'react-icons/fa';
-import ObjectCards from '../../components/ObjectCards/ObjectCards';
-import './ObjectsList.scss'
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import ObjectCards from "../../components/ObjectCards/ObjectCards";
+import "./ObjectsList.scss";
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { CiMenuKebab } from "react-icons/ci";
+import { IoIosMenu } from "react-icons/io";
+import { IoSearchSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 function ObjectsList(props) {
-    return (
-        <div className='ObjectsListContainer'>
-            <ObjectCards/>
-            <Link to="/" className="backBtn"><FaArrowAltCircleLeft/></Link>
+  return (
+    <div className="ObjectsListContainer">
+      {" "}
+      <div className="recapHeader">
+        <div className="left">
+          <IoIosMenu />
+          <p>Parcours</p>
         </div>
-    );
+        <div className="right">
+          <IoSearchSharp />
+          <CiMenuKebab />
+        </div>
+      </div>
+      <div className="sortList">
+        <p>ordonné</p>
+        <p>non ordonné</p>
+      </div>
+      <h3 className="tourNumber">TC 9012</h3>
+      <ObjectCards />
+      <Link to="/" className="backBtn">
+        <FaArrowAltCircleLeft />
+      </Link>
+    </div>
+  );
 }
 
 export default ObjectsList;
